d9a6a85b639d907ee43a7a47035b6ff11c69fa7c
silvamaciel <macieljunio.si@gmail.com>
2025-03-15 17:15:59 -0300

    chore: Criação do projeto Spring Boot inicial

---
+++ backend/src/main/java/com/malu/crmImobiliario/CrmImobiliarioApplication.java
@@ -0,0 +1,13 @@
+package com.malu.crmImobiliario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmImobiliarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmImobiliarioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/malu/crmImobiliario/CrmImobiliarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malu.crmImobiliario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmImobiliarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
