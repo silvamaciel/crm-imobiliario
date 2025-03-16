12d259bc2c8c5c8dabac43b3126e1003220f42d8
silvamaciel <macieljunio.si@gmail.com>
2025-03-16 08:04:53 -0300

    chore: configuração inicial angular modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'leads', loadChildren: () => import('./modules/leads/leads.module').then(m => m.LeadsModule) },
-  { path: '', redirectTo: '/leads', pathMatch: 'full' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/modules/leads/leads.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-leads',
-  template: `<h2>Gestão de Leads</h2>`,
-  styles: [``]
-})
-export class LeadsComponent { }
