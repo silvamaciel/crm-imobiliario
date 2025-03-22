87d487025bbf7e1ea8d2ca8a1e29cec09693bd4f
silvamaciel <macieljunio.si@gmail.com>
2025-03-22 12:18:52 -0300

    feat(service): adicionar operações CRUD ApartamentoSerevice.java

--- backend/src/main/java/com/malu/crmImobiliario/service/ApartamentoService.java
+++ backend/src/main/java/com/malu/crmImobiliario/service/ApartamentoService.java
@@ -1,5 +1,32 @@
 package com.malu.crmImobiliario.service;
 
+import com.malu.crmImobiliario.model.Apartamento;
+import com.malu.crmImobiliario.repository.ApartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class ApartamentoService {
-    
+
+    @Autowired
+    private ApartamentoRepository apartamentoRepository;
+
+    public List<Apartamento> listarTodos() {
+        return apartamentoRepository.findAll();
+    }
+
+    public Optional<Apartamento> buscarPorId(UUID id) {
+        return apartamentoRepository.findById(id);
+    }
+
+    public Apartamento salvar(Apartamento apartamento) {
+        return apartamentoRepository.save(apartamento);
+    }
+
+    public void deletar(UUID id) {
+        apartamentoRepository.deleteById(id);
+    }
 }
